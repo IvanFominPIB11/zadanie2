56d020c5af6e69dc9d525ebc3991a2f36907d703
PIBO0121038 Фомин Иван Константинович <PIBO0121038@sibupk.su>
2023-01-15 17:25:54 +0700

    Добавьте файлы проекта.

---
+++ zadanie2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+using System.Windows.Forms;
+
+namespace zadanie2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string res, txt, name;
+            int year = 2023, age, born;
+            name = Interaction.InputBox("Как Вас зовут?", "Имя");
+            res = Interaction.InputBox("В каком году Вы родились?", "Год рождения");
+            born= Int32.Parse(res);
+            age = year - born;
+            txt = "Вас зовут " + name + " и Вам" +age+ " лет";
+            MessageBox.Show(txt, "Имя и возраст");
+        }
+    }
+}
